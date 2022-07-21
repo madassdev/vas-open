05bc1bfb0ceaa5c8642259972a9aa8c3663d2281
Frank Olayinka Favour <favescsskr@gmail.com>
2022-07-21 10:10:50 +0100

    Transactions filtering

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -16,17 +16,29 @@ class TransactionController extends Controller
         $business = $user->business;
         $query = Transaction::query()->where('business_id', $business->id);
         if ($request->category_id) {
-            $category_id = $request->category_id;
+            $category_id = $request->product_category_id;
             $query = $query->whereHas('product', function ($q) use ($category_id) {
                 $q->where('product_category_id', '=', $category_id);
             });
         }
+        if ($request->product_shortname) {
+            $shortname = $request->product_shortname;
+            $query = $query->whereHas('product', function ($q) use ($shortname) {
+                $q->where('shortname', '=', $shortname);
+            });
+        }
+        if ($request->transaction_status) {
+            $query = $query->where('transaction_status', '=', $request->transaction_status);
+        }
 
-        if($request->start_date && $request->end_date)
-        {
+        if ($request->account_number) {
+            $query = $query->where('account_number', '=', $request->account_number);
+        }
+
+        if ($request->start_date && $request->end_date) {
             $start_date = $request->start_date;
             $end_date = $request->end_date;
-            $query = $query->whereBetween('created_at',[$start_date, $end_date]);
+            $query = $query->whereBetween('created_at', [$start_date, $end_date]);
         }
 
         $transactions = $query->paginate($per_page)->appends(request()->query());
