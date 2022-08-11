a27612c5cc8f94b05fbdd1a66750893dff5f5f60
Johnpaul <polymenjohn1@gmail.com>
2022-08-11 15:21:26 +0100

    updated redis cache prefixes

--- config/database.php
+++ config/database.php
@@ -165,11 +165,11 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => 'vas_reseller',
         ],
 
         'default' => [
-            'url' => env('REDIS_URL'),
+            'url' => env('REDISCLOUD_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
